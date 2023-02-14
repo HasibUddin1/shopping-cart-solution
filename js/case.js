d8a25e30c5c20011d6da823651aaa908780232eb
IconicLuck <uhasib959@gmail.com>
2023-02-15 01:22:40 +0600

    added multiplication add on case plus button

--- js/case.js
+++ js/case.js
@@ -1,8 +1,26 @@
-document.getElementById('btn-case-plus').addEventListener('click', function(){
+function updateCase(isIncrease){
     const caseInputFieldElement = document.getElementById('case-input-field');
     const caseInputFieldString = caseInputFieldElement.value;
     const previousCaseInputField = parseInt(caseInputFieldString);
-    const newCaseInputField = previousCaseInputField + 1;
-
+    let newCaseInputField;
+    if(isIncrease === true){
+        newCaseInputField = previousCaseInputField + 1;
+    }
+    else{
+        newCaseInputField = previousCaseInputField - 1;
+    }
     caseInputFieldElement.value = newCaseInputField;
+
+    return newCaseInputField;
+}
+
+
+document.getElementById('btn-case-plus').addEventListener('click', function(){
+    const caseInputValue = updateCase(true);
+    const totalPrice = caseInputValue * 59;
+    document.getElementById('case-total-amount').innerText = totalPrice;
+})
+
+document.getElementById('btn-case-minus').addEventListener('click', function(){
+    updateCase(false);
 })
